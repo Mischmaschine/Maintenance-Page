a8c073d5b6d5f5e4e1a47c94aa515efe3c935183
Mischmaschine <fixxertrixxer@gmail.com>
2022-09-14 17:45:27 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Maintenance from "./Maintenance";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div
+            className="min-h-screen text-center justify-centert bg-red-400 dark:bg-red-500 w-full">
+            <Maintenance/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Maintenance.tsx
@@ -0,0 +1,14 @@
+export default function Maintenance() {
+    return (
+
+        <div className="h-screen items-center flex m-auto">
+            <div className="w-full">
+                <div className="text-gray-700 font-light text-center">
+                    <h1 className="text-4xl font-bold">Sorry!</h1>
+                    <br/>
+                    <h2 className="text-2xl">Site is temporally down for
+                        maintenance.</h2>
+                </div>
+            </div>
+        </div>)
+}
