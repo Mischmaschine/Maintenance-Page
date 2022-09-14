7642387a3978338fd882ef1b666b0210d4e2a71e
Mischmaschine <fixxertrixxer@gmail.com>
2022-09-14 20:38:52 +0200

    feat: add backdrop-brightness-50

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,8 +5,8 @@ import Maintenance from "./Maintenance";
 function App() {
     return (
         <div
-            className="h-screen w-screen text-center justify-centert bg-[url('https://atlassianblog.wpengine.com/wp-content/uploads/2018/05/scheduled-maintenance-message-examples-and-inspiration0a@3x.png')]">
-            <div className="backdrop-blur-md">
+            className="h-screen w-screen bg-center bg-[url('/pictures/background.png')]">
+            <div className="backdrop-blur-md backdrop-brightness-50">
                 <Maintenance/>
             </div>
         </div>
