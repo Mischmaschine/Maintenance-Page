70ba0fa52452a2c45dbb2381cd5599e21506c505
Mischmaschine <fixxertrixxer@gmail.com>
2022-09-15 13:44:45 +0200

    fix: centered text & div, text highlighted

--- src/components/Maintenance.tsx
+++ src/components/Maintenance.tsx
@@ -1,14 +1,15 @@
 export default function Maintenance() {
     return (
+        <div className="flex h-screen justify-center items-center">
+            <div className="text-center">
+                <div className="text-gray-100 font-light text-center ">
+                    <div className="backdrop-blur-xl backdrop-brightness-95 rounded-3xl p-5 shadow-2xl">
+                        <h1 className="text-7xl font-bold">Sorry!</h1>
+                        <br/>
+                        <h2 className="text-4xl">Site is temporally down for
+                            maintenance.</h2>
+                    </div>
 
-        <div className="h-screen items-center flex m-auto">
-            <div className="w-full">
-                <div className="text-gray-300 font-light text-center">
-                    <h1 className="text-7xl font-bold">Sorry!</h1>
-                    <br/>
-                    <h2 className="text-4xl">Site is temporally down for
-                        maintenance.</h2>
-                    <br/>
                 </div>
             </div>
         </div>)
