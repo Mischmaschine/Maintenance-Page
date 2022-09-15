3e5a11f0fe22f82097e747b83f48d977c3ee9e27
Mischmaschine <fixxertrixxer@gmail.com>
2022-09-15 13:43:40 +0200

    chore: increased backdrop blur

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,7 +6,7 @@ function App() {
     return (
         <div
             className="h-screen w-screen bg-center bg-[url('/pictures/background.png')]">
-            <div className="backdrop-blur-md backdrop-brightness-50">
+            <div className="backdrop-blur-md backdrop-brightness-100">
                 <Maintenance/>
             </div>
         </div>
